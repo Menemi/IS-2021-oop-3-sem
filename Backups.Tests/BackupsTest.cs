e7489a11a0025436f0655f0431fb7d065cf9c528
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-11-19 01:28:21 +0300

    feat: add realization with virtual files

--- Backups.Tests/BackupsTest.cs
+++ Backups.Tests/BackupsTest.cs
@@ -1,30 +1,85 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Backups.Tests
 {
     public class Tests
     {
-        private BackupJob splitBackupJob;
-        private BackupJob singleBackupJob;
+        private BackupService _backupService;
+        private static readonly string _path = @"D:\ITMOre than a university\1Menemi1\Backups\BackupDirectory";
+        private BackupJob _splitBackupJob;
+        private BackupJob _singleBackupJob;
         
         [SetUp]
         public void Setup()
         {
-            splitBackupJob = new BackupJob("split");
-            singleBackupJob = new BackupJob("single");
+            _backupService = new BackupService();
+            _splitBackupJob = _backupService.AddBackupJob("split");
+            _singleBackupJob = _backupService.AddBackupJob("single");
         }
+        
+        // Local test with real files
+
+        // [Test]
+        // public void Test()
+        // {
+        //     _backupService.AddRestorePoint(_splitBackupJob, "RestorePoint", _path);
+        //     _backupService.AddRestorePoint(_singleBackupJob, "RestorePoint", _path);
+        //     var file = new FileInfo(@$"{_path}\Job Objects\name1.txt");
+        //     file.Delete();
+        //     _backupService.AddRestorePoint(_splitBackupJob, "RestorePoint", _path);
+        //     _backupService.AddRestorePoint(_singleBackupJob, "RestorePoint", _path);
+        // }
+        
+        // Tests for git action with virtual files
 
         [Test]
-        public void Test()
+        public void SingleStorageTest()
         {
-            const string path = @"D:\ITMOre than a university\1Menemi1\Backups\BackupDirectory";
-            splitBackupJob.AddRestorePoint("RestorePoint", path);
-            singleBackupJob.AddRestorePoint("RestorePoint", path);
-            var file = new FileInfo(@$"{path}\Job Objects\name1.txt");
-            file.Delete();
-            splitBackupJob.AddRestorePoint("RestorePoint", path);
-            singleBackupJob.AddRestorePoint("RestorePoint", path);
+            var firstFile = new MyFile("name1", DateTime.Now);
+            var secondFile = new MyFile("name2", DateTime.Now);
+            var thirdFile = new MyFile("name3", DateTime.Now);
+            
+            var filesForBackup = new List<MyFile>
+            {
+                firstFile,
+                secondFile,
+                thirdFile
+            };
+            
+            var startResultFiles = _backupService.AddVirtualRestorePoint(_singleBackupJob, filesForBackup);
+            var startResultFilesCount = startResultFiles[0].Count;
+            filesForBackup.Remove(firstFile);
+            var resultFiles = _backupService.AddVirtualRestorePoint(_singleBackupJob, filesForBackup);
+            var resultFilesCount = resultFiles[0].Count;
+            
+            Assert.True(_backupService.GetRestorePoints().Count == 2);
+            Assert.True(startResultFilesCount == 3);
+            Assert.True(resultFilesCount == 2);
+        }
+        
+        [Test]
+        public void SplitStorageTest()
+        {
+            var firstFile = new MyFile("name1", DateTime.Now);
+            var secondFile = new MyFile("name2", DateTime.Now);
+            var thirdFile = new MyFile("name3", DateTime.Now);
+            
+            var filesForBackup = new List<MyFile>
+            {
+                firstFile,
+                secondFile,
+                thirdFile
+            };
+            
+            var startResultFilesCount = _backupService.AddVirtualRestorePoint(_splitBackupJob, filesForBackup).Count;
+            filesForBackup.Remove(firstFile);
+            var resultFilesCount = _backupService.AddVirtualRestorePoint(_splitBackupJob, filesForBackup).Count;
+            
+            Assert.True(_backupService.GetRestorePoints().Count == 2);
+            Assert.True(startResultFilesCount == 3);
+            Assert.True(resultFilesCount == 2);
         }
     }
 }
---
+++ Backups/MyFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Backups
+{
+    public class MyFile
+    {
+        private string _name;
+
+        private DateTime _creationTime;
+
+        public MyFile(string name, DateTime creationTime)
+        {
+            _name = name;
+            _creationTime = creationTime;
+        }
+    }
+}
