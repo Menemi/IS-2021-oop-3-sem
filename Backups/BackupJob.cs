5a3721c2a9b72f0377cdd85ccc7d38a86de00548
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-11-18 23:26:15 +0300

    feat: add logic of backup woking
    backup job + restore points

---
+++ Backups/BackupJob.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace Backups
+{
+    public class BackupJob
+    {
+        private static string _path;
+
+        private static int _idCounter = 1;
+
+        private string _storageType;
+
+        private int _id;
+
+        public BackupJob(string storageType, string path = @"D:\ITMOre than a university\1Menemi1\Backups\BackupDirectory")
+        {
+            _id = _idCounter++;
+            _storageType = storageType;
+            _path = path;
+        }
+
+        public void AddRestorePoint(string restorePointName, string backupPlace)
+        {
+            var directory = new DirectoryInfo(_path);
+            var directory2 = new DirectoryInfo(@$"{_path}\Job Objects");
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            if (!directory2.Exists)
+            {
+                directory2.Create();
+            }
+
+            var restorePoint = new RestorePoint(restorePointName, _storageType, backupPlace);
+        }
+    }
+}
---
+++ Backups/RestorePoint.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.IO.Compression;
+using Backups.Exceptions;
+
+namespace Backups
+{
+    public class RestorePoint
+    {
+        private static int _idCounter = 1;
+
+        private static string _path = @"D:\ITMOre than a university\1Menemi1\Backups\BackupDirectory";
+
+        private int _id;
+
+        public RestorePoint(string restorePointName, string storageType, string backupPlace)
+        {
+            _id = _idCounter++;
+            _path = backupPlace;
+
+            var directory = new DirectoryInfo(@$"{_path}\{restorePointName}{_id}");
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            switch (storageType.ToLower())
+            {
+                case "split":
+                    Split(restorePointName, _path);
+                    break;
+                case "single":
+                    Single(restorePointName, _path);
+                    break;
+                default:
+                    throw new WrongStorageTypeException();
+            }
+        }
+
+        private void Single(string restorePointName, string backupPlace)
+        {
+            var jobObjectsDirectory = new DirectoryInfo(@$"{backupPlace}\Job Objects");
+            if (jobObjectsDirectory.GetFiles().Length == 0)
+            {
+                return;
+            }
+
+            var restorePointDirectory = restorePointName + _id;
+            var archiveName = "Files_" + _id;
+            var startPath = @$"{backupPlace}\Job Objects";
+            var zipPath =
+                @$"{backupPlace}\{restorePointDirectory}\{archiveName}.zip";
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+        }
+
+        private void Split(string restorePointName, string backupPlace)
+        {
+            var restorePointDirectory = restorePointName + _id;
+            var jobObjectsDirectory = new DirectoryInfo(@$"{backupPlace}\Job Objects");
+
+            foreach (var file in jobObjectsDirectory.GetFiles())
+            {
+                var archiveName =
+                    Path.GetFileNameWithoutExtension(@$"{backupPlace}/Job Objects/{file.Name}") + "_" + _id;
+
+                var pathFileToAdd = @$"{backupPlace}\Job Objects\{file.Name}";
+                var zipPath = @$"{backupPlace}\{restorePointDirectory}\{archiveName}.zip";
+
+                using var zipArchive = ZipFile.Open(zipPath, ZipArchiveMode.Create);
+                zipArchive.CreateEntryFromFile(pathFileToAdd, file.Name);
+            }
+        }
+    }
+}
