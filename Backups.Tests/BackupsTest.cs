8e3ea36ec3a52f9c4ebf562d338bc2ae88be1316
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-11-18 23:25:19 +0300

    test: add local tests
    useless for git action

---
+++ Backups.Tests/BackupsTest.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Backups.Tests
+{
+    public class Tests
+    {
+        private BackupJob splitBackupJob;
+        private BackupJob singleBackupJob;
+        
+        [SetUp]
+        public void Setup()
+        {
+            splitBackupJob = new BackupJob("split");
+            singleBackupJob = new BackupJob("single");
+        }
+
+        [Test]
+        public void Test()
+        {
+            const string path = @"D:\ITMOre than a university\1Menemi1\Backups\BackupDirectory";
+            splitBackupJob.AddRestorePoint("RestorePoint", path);
+            singleBackupJob.AddRestorePoint("RestorePoint", path);
+            var file = new FileInfo(@$"{path}\Job Objects\name1.txt");
+            file.Delete();
+            splitBackupJob.AddRestorePoint("RestorePoint", path);
+            singleBackupJob.AddRestorePoint("RestorePoint", path);
+        }
+    }
+}
