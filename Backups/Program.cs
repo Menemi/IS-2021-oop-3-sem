a90f275ae57a3b667391918505f59d1e96191d63
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-11-29 18:31:19 +0300

    feat: add local tests

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Backups
+﻿using Backups.Interfaces;
+
+namespace Backups
 {
     internal class Program
     {
         private static void Main()
         {
+            // Local test
+            const string generalPath = @"D:\ITMOre than a university\1Menemi1\Backups\BackupDirectory";
+            IStorageSaver singleSaver = new Single();
+            IStorageSaver splitSaver = new Split();
+            IBackupSaver localSaver = new LocalSaver();
+            var fileSystem = new FileSystem(generalPath);
+
+            var splitBackupJob = new BackupJob(splitSaver, fileSystem);
+            var singleBackupJob = new BackupJob(singleSaver, fileSystem);
+
+            var file11 = splitBackupJob.AddJobObject(@"C:\Users\danil\Desktop\name1.txt");
+            var file12 = splitBackupJob.AddJobObject(@"C:\Users\danil\Desktop\name2.txt");
+            var file13 = splitBackupJob.AddJobObject(@"C:\Users\danil\Desktop\name3.txt");
+            var file21 = singleBackupJob.AddJobObject(@"C:\Users\danil\Desktop\name1.txt");
+            var file22 = singleBackupJob.AddJobObject(@"C:\Users\danil\Desktop\name2.txt");
+            var file23 = singleBackupJob.AddJobObject(@"C:\Users\danil\Desktop\name3.txt");
+
+            singleBackupJob.CreateRestorePoint(localSaver, generalPath, "SingleRestorePoint");
+            splitBackupJob.CreateRestorePoint(localSaver, generalPath, "SplitRestorePoint");
+            splitBackupJob.DeleteJobObject(file13);
+            singleBackupJob.DeleteJobObject(file23);
+            singleBackupJob.CreateRestorePoint(localSaver, generalPath, "SingleRestorePoint");
+            splitBackupJob.CreateRestorePoint(localSaver, generalPath, "SplitRestorePoint");
         }
     }
 }
