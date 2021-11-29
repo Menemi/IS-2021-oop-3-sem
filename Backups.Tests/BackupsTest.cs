b4b24de5e8a0f4b675f61f7e1663820666623a7c
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-11-29 18:35:16 +0300

    refactor: delete usless libraries

--- Backups.Tests/BackupsTest.cs
+++ Backups.Tests/BackupsTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Backups.Interfaces;
+﻿using Backups.Interfaces;
 using NUnit.Framework;
 
 namespace Backups.Tests
--- Backups/BackupJob.cs
+++ Backups/BackupJob.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Net;
 using Backups.Exceptions;
 using Backups.Interfaces;
 
