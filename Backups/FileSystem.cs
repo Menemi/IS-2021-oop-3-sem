c5dbf472d3df308b8b7aab059a8825d2d21c1b28
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-11-29 18:31:53 +0300

    feat: add file system

---
+++ Backups/FileSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using Backups.Exceptions;
+
+namespace Backups
+{
+    public class FileSystem
+    {
+        private List<DirectoryInfo> _restorePointDirectories;
+
+        public FileSystem(string generalPath)
+        {
+            GeneralPath = generalPath;
+            _restorePointDirectories = new List<DirectoryInfo>();
+        }
+
+        public string GeneralPath { get; }
+
+        public ReadOnlyCollection<DirectoryInfo> GetRestorePointDirectories()
+        {
+            return _restorePointDirectories.AsReadOnly();
+        }
+
+        public DirectoryInfo AddRestorePointDirectory(string restorePointPath)
+        {
+            var directory = new DirectoryInfo(restorePointPath);
+            if (directory.Exists)
+            {
+                throw new BackupsException("Directory already exists");
+            }
+
+            directory.Create();
+
+            _restorePointDirectories.Add(directory);
+            return directory;
+        }
+    }
+}
