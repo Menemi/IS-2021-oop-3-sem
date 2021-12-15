56960e64f6668a59be6a80d739ccdad78de31049
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-15 03:18:50 +0300

    feat: add percents 4 deposit account & some code tests

---
+++ Banks/PercentOfTheAmount.cs
@@ -0,0 +1,18 @@
+﻿namespace Banks
+{
+    public class PercentOfTheAmount
+    {
+        public PercentOfTheAmount(float lowerBound, float upperBound, float percent)
+        {
+            LowerBound = lowerBound;
+            UpperBound = upperBound;
+            Percent = percent;
+        }
+
+        public float LowerBound { get; }
+
+        public float UpperBound { get; }
+
+        public float Percent { get; }
+    }
+}
--- Banks/Program.cs
+++ Banks/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Banks
+﻿using System;
+using System.Collections.Generic;
+using Banks.AccountTypes;
+
+namespace Banks
 {
     internal static class Program
     {
         private static void Main()
         {
+            var list = new List<PercentOfTheAmount>()
+            {
+                new PercentOfTheAmount(0, float.MaxValue, 3),
+            };
+
+            var bank = new Bank("Tinkoff", list, 3, 10000, 10000, 10000, 100, new DateTime(2022, 1, 1));
+            AccountBuilder builder = new DebitAccount();
+            var debit = bank.CreateAccount(builder, 0);
+            builder = new CreditAccount();
+            var credit = bank.CreateAccount(builder, 0);
+            builder = new DepositAccount();
+            var deposit = bank.CreateAccount(builder, 100000);
+
+            Console.Read();
         }
     }
 }
