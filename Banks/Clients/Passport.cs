878d37d8383494fa469a5f3cad1e63eda825273e
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-15 03:17:48 +0300

    feat: add passport 4 client

---
+++ Banks/Clients/Passport.cs
@@ -0,0 +1,15 @@
+﻿namespace Banks
+{
+    public class Passport
+    {
+        public Passport(int series, int number)
+        {
+            Series = series;
+            Number = number;
+        }
+
+        public int Series { get; set; }
+
+        public int Number { get; set; }
+    }
+}
