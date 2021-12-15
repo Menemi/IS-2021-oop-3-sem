40d95a7e26dfb969fcbc96af3534d60369548748
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-15 03:17:14 +0300

    feat: add client builder

---
+++ Banks/Clients/Client.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Banks.AccountTypes;
+
+namespace Banks
+{
+    public class Client : ClientBuilder
+    {
+        private List<Account> _accountsList;
+
+        public Client(string name, string surname)
+        {
+            _accountsList = new List<Account>();
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public string Address { get; private set; } = null;
+
+        public Passport Passport { get; private set; } = null;
+
+        public bool Doubtful { get; private set; } = true;
+
+        public override void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public override void SetSurname(string surname)
+        {
+            Surname = surname;
+        }
+
+        public override void SetAddress(string address)
+        {
+            Address = address;
+            if (Passport != null)
+            {
+                Doubtful = false;
+            }
+        }
+
+        public override void SetPassport(Passport passport)
+        {
+            Passport = passport;
+            if (Address != null)
+            {
+                Doubtful = false;
+            }
+        }
+
+        public void AddNewAccount(Account account)
+        {
+            _accountsList.Add(account);
+        }
+    }
+}
---
+++ Banks/Clients/ClientBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Banks.AccountTypes;
+
+namespace Banks
+{
+    public abstract class ClientBuilder
+    {
+        public Client Client { get; private set; }
+
+        public void CreateNewClient(string name, string surname)
+        {
+            Client = new Client(name, surname);
+        }
+
+        public abstract void SetName(string name);
+
+        public abstract void SetSurname(string surname);
+
+        public abstract void SetAddress(string address);
+
+        public abstract void SetPassport(Passport passport);
+    }
+}
