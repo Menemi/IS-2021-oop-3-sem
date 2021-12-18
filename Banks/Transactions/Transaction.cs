b4da5b801eef971be5b4d4c613b8917aa96e0152
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-18 03:05:26 +0300

    refactor: general transaction class

--- Banks/Transactions/Transaction.cs
+++ Banks/Transactions/Transaction.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using Banks.AccountTypes;
 
 namespace Banks
 {
     public abstract class Transaction
     {
-        public Transaction(float transactionAmount)
+        public Transaction(int id, Account sender, Account recipient, float amount)
         {
+            Id = id;
+            TransactionAmount = amount;
+            Sender = sender;
+            Recipient = recipient;
             TransactionTime = DateTime.Now;
-            TransactionAmount = transactionAmount;
         }
 
-        public DateTime TransactionTime { get; set; }
+        public int Id { get; }
 
-        public float TransactionAmount { get; set; }
+        public float TransactionAmount { get; }
+
+        public Account Sender { get; }
+
+        public Account Recipient { get; }
+
+        public DateTime TransactionTime { get; }
     }
 }
