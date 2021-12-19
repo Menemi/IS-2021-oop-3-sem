f794517243c6f2d6abc7f35e83a835c46bfae1ec
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-20 00:11:17 +0300

    feat: add time machine

---
+++ Banks/TimeMachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banks
+{
+    public class TimeMachine
+    {
+        public void TimeRewind(CentralBank centralBank, DateTime dateToRewind)
+        {
+            centralBank.NotifyObservers(dateToRewind);
+        }
+    }
+}
