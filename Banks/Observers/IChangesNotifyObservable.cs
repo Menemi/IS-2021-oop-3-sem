f47460f700e0eff0cf0b4b318cd707b687a18f0f
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-20 23:46:40 +0300

    feat: add observer 4 notifies about changes

---
+++ Banks/Observers/IChangesNotifyObservable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Banks.AccountTypes;
+using Banks.BankMessages;
+
+namespace Banks.Observers
+{
+    public interface IChangesNotifyObservable
+    {
+        void RegisterObserver(Account account, IChangesNotifyObserver accountsObserver);
+
+        void RemoveObserver(Account account, IChangesNotifyObserver accountsObserver);
+
+        void NotifyObservers(Dictionary<Account, IChangesNotifyObserver> observers, double amount, IBankMessage message);
+    }
+}
---
+++ Banks/Observers/IChangesNotifyObserver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Banks.BankMessages;
+
+namespace Banks.Observers
+{
+    public interface IChangesNotifyObserver
+    {
+        void Update(string message);
+    }
+}
