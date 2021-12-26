b97249ae302b9d538ec47555e710a2a2d1790845
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-27 02:05:35 +0300

    feat: logging methods

---
+++ BackupsExtra/Logging/ConsoleLogging.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace BackupsExtra.Logging
+{
+    public class ConsoleLogging : ILogging
+    {
+        public void CreateLog(bool isTimecodeOn, string message)
+        {
+            if (isTimecodeOn)
+            {
+                using var logger = new LoggerConfiguration()
+                    .WriteTo.Console(outputTemplate: "{Timestamp:yy.MM.dd HH:mm:ss}: {Message}{NewLine}{Exception}")
+                    .CreateLogger();
+                logger.Information(message);
+            }
+            else
+            {
+                using var logger = new LoggerConfiguration()
+                    .WriteTo.Console(outputTemplate: "{Message}{NewLine}{Exception}")
+                    .CreateLogger();
+                logger.Information(message);
+            }
+        }
+    }
+}
