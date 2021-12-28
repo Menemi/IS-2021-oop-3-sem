014f7d1f0c31aa70425494fb9f67cb825b205785
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-29 01:57:29 +0300

    feat: add all new methods
    мердж, восстановление, автоудаление и так далее

--- BackupsExtra/ComplementedBackupJob.cs
+++ BackupsExtra/ComplementedBackupJob.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using Backups;
 using Backups.Interfaces;
 using BackupsExtra.Logging;
+using BackupsExtra.Merge;
+using BackupsExtra.Recovery;
+using BackupsExtra.RemoveOfRestorePoints;
 
 namespace BackupsExtra
 {
@@ -20,16 +24,49 @@ namespace BackupsExtra
 
         private FileSystem _fileSystem;
 
+        // by count / date / hybrid
+        private IRemoveRestorePoint _removeRestorePoint;
+
+        // virtual / local
+        private IRestorePointRemover _restorePointRemover;
+
+        // to origin / custom place
+        private IRecoveryPlace _recoveryPlace;
+
+        // virtual / local
+        private IRecoveryType _recoveryType;
+
+        // virtual / local
+        private IMergeType _mergeType;
+
+        private ILogging _logger;
+
+        private bool _isAllLimitsOn;
+
         public ComplementedBackupJob(
             IStorageSaver storageSaver,
             FileSystem fileSystem,
-            DataService dataService)
+            IRemoveRestorePoint removeRestorePoint,
+            IRestorePointRemover restorePointRemover,
+            DataService dataService,
+            IRecoveryPlace recoveryPlace,
+            IRecoveryType recoveryType,
+            IMergeType mergeType,
+            ILogging logger,
+            bool isAllLimitsOn)
             : base(storageSaver, fileSystem)
         {
             _restorePoints = new List<RestorePoint>();
             _jobObjects = new List<FileInfo>();
             _storageSaver = storageSaver;
             _fileSystem = fileSystem;
+            _isAllLimitsOn = isAllLimitsOn;
+            _mergeType = mergeType;
+            _recoveryPlace = recoveryPlace;
+            _recoveryType = recoveryType;
+            _logger = logger;
+            _restorePointRemover = restorePointRemover;
+            _removeRestorePoint = removeRestorePoint;
             Name = storageSaver.GetType().ToString();
             dataService.AddBackupJob(this);
         }
@@ -42,23 +79,65 @@ namespace BackupsExtra
             get { return _jobObjects.Select(file => Path.Combine(file.Directory.ToString(), file.Name)).ToArray(); }
         }
 
-        public FileInfo AddJobObject(ILogging logging, bool isTimecodeOn, string filePath)
+        public ReadOnlyCollection<RestorePoint> GetNewRestorePoints()
+        {
+            return _restorePoints.AsReadOnly();
+        }
+
+        public bool GetIsAllLimitsOn()
+        {
+            return _isAllLimitsOn;
+        }
+
+        public void RemoveRestorePoints(List<RestorePoint> restorePoints, bool isTimecodeOn)
+        {
+            foreach (var restorePoint in restorePoints)
+            {
+                _restorePoints.Remove(restorePoint);
+
+                var restorePointDirectory = new DirectoryInfo(restorePoint.Path);
+                _logger.CreateLog(
+                    isTimecodeOn,
+                    $"Restore point '{restorePointDirectory.Name}{restorePoint.Id}' was successfully deleted");
+            }
+        }
+
+        public void SetRemoveRestorePoint(IRemoveRestorePoint removeRestorePoint)
+        {
+            _removeRestorePoint = removeRestorePoint;
+        }
+
+        public void SetRestorePointRemover(IRestorePointRemover restorePointRemover)
+        {
+            _restorePointRemover = restorePointRemover;
+        }
+
+        public void SetRecoveryPlace(IRecoveryPlace recoveryPlace)
+        {
+            _recoveryPlace = recoveryPlace;
+        }
+
+        public void SetRecoveryType(IRecoveryType recoveryType)
+        {
+            _recoveryType = recoveryType;
+        }
+
+        public FileInfo AddJobObject(bool isTimecodeOn, string filePath)
         {
             var file = new FileInfo(filePath);
             _jobObjects.Add(file);
 
-            logging.CreateLog(isTimecodeOn, $"Storage '{file.Name}' was successfully created in '{Name}' backup job");
+            _logger.CreateLog(isTimecodeOn, $"Storage '{file.Name}' was successfully created in '{Name}' backup job");
             return file;
         }
 
-        public void DeleteJobObject(ILogging logging, bool isTimecodeOn, FileInfo file)
+        public void DeleteJobObject(bool isTimecodeOn, FileInfo file)
         {
-            logging.CreateLog(isTimecodeOn, $"Storage '{file.Name}' was successfully deleted from '{Name}' backup job");
+            _logger.CreateLog(isTimecodeOn, $"Storage '{file.Name}' was successfully deleted from '{Name}' backup job");
             _jobObjects.Remove(file);
         }
 
         public RestorePoint CreateRestorePoint(
-            ILogging logging,
             IBackupSaver backupSaver,
             bool isTimecodeOn,
             string restorePointPath,
@@ -70,11 +149,33 @@ namespace BackupsExtra
             _storageSaver.SaveStorage(backupSaver, _fileSystem, _jobObjects, restorePoint);
 
             var restorePointDirectory = new DirectoryInfo(restorePoint.Path);
-            logging.CreateLog(
+            _logger.CreateLog(
                 isTimecodeOn,
                 $"Restore point '{restorePointDirectory.Name}{restorePoint.Id}' was successfully created");
 
+            _removeRestorePoint.RemoveRestorePoint(_restorePointRemover, this, isTimecodeOn);
             return restorePoint;
+        }
+
+        public void Recovery(RestorePoint restorePoint, bool isTimecodeOn, string pathToRecovery = null)
+        {
+            _recoveryPlace.Recovery(_recoveryType, restorePoint, pathToRecovery);
+
+            var restorePointDirectory = new DirectoryInfo(restorePoint.Path);
+            _logger.CreateLog(
+                isTimecodeOn,
+                $"Recovery of restore point '{restorePointDirectory.Name}{restorePoint.Id}' was successfully done!");
+        }
+
+        public void Merge(RestorePoint oldRestorePoint, RestorePoint newRestorePoint, bool isTimecodeOn)
+        {
+            _mergeType.Merge(this, oldRestorePoint, newRestorePoint, isTimecodeOn);
+
+            var oldRestorePointDirectory = new DirectoryInfo(oldRestorePoint.Path);
+            var newRestorePointDirectory = new DirectoryInfo(newRestorePoint.Path);
+            _logger.CreateLog(
+                isTimecodeOn,
+                $"Merge of restore point '{oldRestorePointDirectory.Name}{oldRestorePoint.Id}' was successfully done into restore point '{newRestorePointDirectory.Name}{newRestorePoint.Id}'!");
         }
     }
 }
