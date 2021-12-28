c2257664ee983ce43c597974be68aeb49fd42f2e
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-29 01:56:44 +0300

    feat: add exception class

---
+++ BackupsExtra/Exception/BackupsExtraException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BackupsExtra.Exception
+{
+    public class BackupsExtraException : System.Exception
+    {
+        public BackupsExtraException()
+        {
+        }
+
+        public BackupsExtraException(string message)
+            : base(message)
+        {
+        }
+
+        public BackupsExtraException(string message, System.Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
