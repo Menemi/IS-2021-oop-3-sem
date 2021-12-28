164ede84f9505ada2c74dbd6515fb901f2b1e49e
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-29 01:57:41 +0300

    feat: add local tests

--- BackupsExtra/Program.cs
+++ BackupsExtra/Program.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
+using System.IO;
 using Backups;
 using Backups.Interfaces;
 using BackupsExtra.Logging;
+using BackupsExtra.Merge;
+using BackupsExtra.Recovery;
+using BackupsExtra.RemoveOfRestorePoints;
 using Single = Backups.Single;
 
 namespace BackupsExtra
@@ -10,20 +14,47 @@ namespace BackupsExtra
     {
         private static void Main()
         {
-            var dataService = new DataService();
-
-            // dataService.LoadData();
-            // Console.WriteLine();
             const string generalPath = @"D:\ITMOre than a university\1Menemi1\BackupsExtra\BackupDirectory";
+
             IStorageSaver singleSaver = new Single();
             IStorageSaver splitSaver = new Split();
+            var fileSystem = new FileSystem(generalPath);
+            IRemoveRestorePoint dateRemove = new RemoveByDate();
+            IRemoveRestorePoint hybridRemove = new RemoveByHybrid();
+            IRemoveRestorePoint numberRemove = new RemoveByNumber();
+            IRestorePointRemover localRemove = new LocalRemove();
+            var dataService = new DataService();
+            IRecoveryPlace originalPlace = new OriginalPlace();
+            IRecoveryPlace customPlace = new CustomPlace();
+            IRecoveryType localRecovery = new LocalRecovery();
+            IMergeType localMerge = new LocalMerge();
             ILogging fileLogging = new FileLogging();
             ILogging consoleLogging = new ConsoleLogging();
             IBackupSaver localSaver = new LocalSaver();
-            var fileSystem = new FileSystem(generalPath);
 
-            var splitBackupJob = new ComplementedBackupJob(splitSaver, fileSystem, dataService);
-            var singleBackupJob = new ComplementedBackupJob(singleSaver, fileSystem, dataService);
+            // dataService.LoadData();
+            var splitBackupJob = new ComplementedBackupJob(
+                splitSaver,
+                fileSystem,
+                numberRemove, // dateRemove / hybridRemove
+                localRemove,
+                dataService,
+                customPlace, // originalPlace
+                localRecovery,
+                localMerge,
+                fileLogging,
+                false);
+            var singleBackupJob = new ComplementedBackupJob(
+                singleSaver,
+                fileSystem,
+                numberRemove, // dateRemove / hybridRemove
+                localRemove,
+                dataService,
+                customPlace, // originalPlace
+                localRecovery,
+                localMerge,
+                fileLogging,
+                false);
 
             const string filePath1 = @"C:\Users\danil\Desktop\name1.txt";
             const string filePath2 = @"C:\Users\danil\Desktop\name2.txt";
@@ -41,21 +72,30 @@ namespace BackupsExtra
                 splitBackupJob.FileExsistingCheck(filePath);
             }
 
-            var file11 = splitBackupJob.AddJobObject(fileLogging, true, filePath1);
-            var file12 = splitBackupJob.AddJobObject(fileLogging, true, filePath2);
-            var file13 = splitBackupJob.AddJobObject(fileLogging, true, filePath3);
-            var file21 = singleBackupJob.AddJobObject(fileLogging, true, filePath1);
-            var file22 = singleBackupJob.AddJobObject(fileLogging, true, filePath2);
-            var file23 = singleBackupJob.AddJobObject(fileLogging, true, filePath3);
+            var file11 = splitBackupJob.AddJobObject(true, filePath1);
+            var file12 = splitBackupJob.AddJobObject(true, filePath2);
+            var file13 = splitBackupJob.AddJobObject(true, filePath3);
+            var file21 = singleBackupJob.AddJobObject(true, filePath1);
+            var file22 = singleBackupJob.AddJobObject(true, filePath2);
+            var file23 = singleBackupJob.AddJobObject(true, filePath3);
 
-            singleBackupJob.CreateRestorePoint(fileLogging, localSaver, true, generalPath, "SingleRestorePoint");
-            splitBackupJob.CreateRestorePoint(fileLogging, localSaver, true, generalPath, "SplitRestorePoint");
+            var restorePoint1 = singleBackupJob.CreateRestorePoint(localSaver, true, generalPath, "SingleRestorePoint");
+            var restorePoint2 = splitBackupJob.CreateRestorePoint(localSaver, true, generalPath, "SplitRestorePoint");
             splitBackupJob.FileExsistingCheck(filePath3);
-            splitBackupJob.DeleteJobObject(fileLogging, true, file13);
-            singleBackupJob.DeleteJobObject(fileLogging, true, file23);
-            singleBackupJob.CreateRestorePoint(fileLogging, localSaver, true, generalPath, "SingleRestorePoint");
-            splitBackupJob.CreateRestorePoint(fileLogging, localSaver, true, generalPath, "SplitRestorePoint");
+            splitBackupJob.DeleteJobObject(true, file13);
+            singleBackupJob.DeleteJobObject(true, file23);
+            var restorePoint3 = singleBackupJob.CreateRestorePoint(localSaver, true, generalPath, "SingleRestorePoint");
+            var restorePoint4 = splitBackupJob.CreateRestorePoint(localSaver, true, generalPath, "SplitRestorePoint");
+            var restorePoint5 = singleBackupJob.CreateRestorePoint(localSaver, true, generalPath, "SingleRestorePoint");
+            var restorePoint6 = splitBackupJob.CreateRestorePoint(localSaver, true, generalPath, "SplitRestorePoint");
 
+            singleBackupJob.Merge(restorePoint1, restorePoint3, true);
+            singleBackupJob.Merge(restorePoint3, restorePoint2, true);
+            singleBackupJob.Merge(restorePoint2, restorePoint4, true);
+
+            var directory = new DirectoryInfo("D:/ITMOre than a university/1Menemi1/BackupsExtra/testDirectory");
+            directory.Create();
+            singleBackupJob.Recovery(restorePoint3, true, "D:/ITMOre than a university/1Menemi1/BackupsExtra/testDirectory");
             dataService.SaveData();
         }
     }
