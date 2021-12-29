52d5ef035b619c011b38dfcab1ce9248ce6870fe
Danya <47279903+1Menemi1@users.noreply.github.com>
2021-12-29 19:35:12 +0300

    refactor: rename classes

---
+++ BackupsExtra/Recovery/CustomPlacementRecovery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Backups;
+
+namespace BackupsExtra.Recovery
+{
+    public class CustomPlacementRecovery : IRecoveryPlacement
+    {
+        public void Recovery(IRecoveryProcessMethod recoveryProcessMethod, RestorePoint restorePoint, string pathToRecovery)
+        {
+            var pathsToRecovery = new List<string>();
+            foreach (var repository in restorePoint.GetRepositories())
+            {
+                foreach (var file in repository.GetStorageList())
+                {
+                    pathsToRecovery.Add(pathToRecovery);
+                }
+            }
+
+            recoveryProcessMethod.Recovery(restorePoint, pathsToRecovery);
+        }
+    }
+}
